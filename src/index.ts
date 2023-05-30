0683b8b3762e0de2f1e1c3d21a367ee76b67031d
coyotespike <timothy.roy@protonmail.ch>
2023-05-29 20:06:59 -0500

    Basic project with working aliases

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+require("module-alias/register");
+
+import { hello as utilsHello } from "@utils";
+import { hello as agentsHello } from "@agents";
+console.log(utilsHello, agentsHello);
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type BabyAGIConfig = {
+  name: string;
+  objective: string;
+  initialTask: string;
+  llm: LLMModels;
+  root: string;
+};
+
+export enum LLMModels {
+  GPT3 = "gpt-3.5-turbo",
+  GPT4 = "gpt-4",
+  GPT432k = "gpt-4-32k",
+}
+
+export type LLMModelDetails = {
+  name: LLMModels;
+  inputCostPer1KTokens: number;
+  outputCostPer1KTokens: number;
+  maxLength: number;
+};
+
+export type Task = {
+  taskId: number;
+  taskName: string;
+};
