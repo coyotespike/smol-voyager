1cd717ac3d6b24e5c2a3c139a80cccef67cecdb5
coyotespike <timothy.roy@protonmail.ch>
2023-05-29 20:28:15 -0500

    Add esbuild and init

---
+++ src/cli/commands/init/index.ts
@@ -0,0 +1,73 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import fs from "node:fs";
+import path from "node:path";
+import { BabyAGIConfig, LLMModels } from "@types";
+
+export const makeConfigTemplate = (config?: BabyAGIConfig): BabyAGIConfig => {
+  return {
+    name: config?.name ?? "",
+    objective: config?.objective ?? "",
+    initialTask: config?.initialTask ?? "",
+    llm: LLMModels.GPT4,
+    root: "./",
+  };
+};
+
+export const init = async (config: BabyAGIConfig = makeConfigTemplate()) => {
+  const configPath = path.join(config.root, "babyagi.config.json");
+
+  if (fs.existsSync(configPath)) {
+    const questions = [
+      {
+        type: "confirm",
+        name: "continue",
+        message:
+          "A babyagi.config.json file already exists in this location. The existing configuration will be overwritten. Do you want to continue? ",
+        default: false,
+      },
+    ];
+
+    const answers = await inquirer.prompt(questions);
+    if (!answers.continue) {
+      process.exit(0);
+    }
+  }
+
+  const questions = [
+    {
+      type: "input",
+      name: "name",
+      message: chalk.yellow(`Enter the name of your agent:`),
+      default: config.name,
+    },
+    {
+      type: "input",
+      name: "objective",
+      message: chalk.yellow(`Enter your agent's objective:`),
+      default: config.objective,
+    },
+  ];
+
+  const { name, objective, initialTask, llm } = await inquirer.prompt(
+    questions
+  );
+
+  const newConfig = makeConfigTemplate({
+    ...config,
+    name,
+    objective,
+    initialTask,
+    llm,
+  });
+
+  fs.writeFileSync(
+    path.join(newConfig.root, "babyagi.config.json"),
+    JSON.stringify(newConfig, null, 2),
+    "utf-8"
+  );
+
+  console.log(
+    chalk.green("BabyAGI initialized. Run `babyagi start` to start the agent.")
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-import { hello as utilsHello } from "@utils"; import { hello as agentsHello } from "@agents"; console.log(utilsHello, agentsHello);
+import { hello as utilsHello } from "@utils";
+import { hello as agentsHello } from "@agents";
+console.log(utilsHello, agentsHello);
+
+import fs from "node:fs/promises";
+import { Command } from "commander";
+import { spinnerError, stopSpinner } from "./cli/spinner.js";
+import { init } from "./cli/commands/init/index.js";
+// import { run } from "./cli/commands/run/index.js";
+import { BabyAGIConfig } from "@types";
+
+const program = new Command();
+program.description("BabyAGI CLI Tool");
+program.version("0.0.3");
+
+program
+  .command("init")
+  .description(
+    "Initialize project by creating a `babyagi.config.json` file in the current directory."
+  )
+  .action(async () => {
+    try {
+      const config: BabyAGIConfig = JSON.parse(
+        await fs.readFile("./babyagi.config.json", "utf8")
+      );
+      init(config);
+    } catch (e) {
+      init();
+    }
+  });
