5aa35025a6bf724dc913e54d092850564a5dcfce
coyotespike <timothy.roy@protonmail.ch>
2023-06-02 18:18:03 -0500

    Transform arguments to call toolModules, use .ts

--- src/tools/runTool.ts
+++ src/tools/runTool.ts
@@ -4,18 +4,24 @@ async function runTool(toolName: string, args: any) {
   try {
     // Dynamically import the tool's module
     // Written to work with compiled JS in dist/
-    const modulePath = path.join(__dirname, `./tools/${toolName}.js`);
+    const modulePath = path.join(__dirname, `./tools/${toolName}.ts`);
     console.log(`Importing tool module from ${modulePath}`);
     const toolModule = await import(modulePath);
 
-    // Call the tool's function with the provided arguments
-    const result = await toolModule[toolName](args);
+    // Transform the args array into an object
+    const argsObject = args.reduce((obj: any, arg: any) => {
+      obj[arg.name] = arg.value;
+      return obj;
+    }, {});
+
+    // Call the tool's function with the transformed arguments
+    const result = await toolModule[toolName](argsObject);
 
     // Return the result
-    return result;
+    return { success: true, result };
   } catch (error) {
     console.error(`Failed to run tool "${toolName}": ${error}`);
-    throw error;
+    return { success: false, result: error };
   }
 }
 
